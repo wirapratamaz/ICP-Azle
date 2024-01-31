1c4bd6273d3b28c74a6d5fa228aaa30338644955
wirapratamaz <wirabagus185@gmail.com>
2024-01-31 16:14:56 +0800

    Fix problem about hashcode depedency

--- src/dfinity_js_backend/src/index.ts
+++ src/dfinity_js_backend/src/index.ts
@@ -1,1 +1,8 @@
-// canister code goes here
+//Backend for logic core canister
+import { query, update, text, Record, StableBTreeMap, Variant, Vec, None, Some, Ok, Err, ic, Principal, Opt, nat64, Duration, Result, bool, Canister } from "azle";
+import {
+    Ledger, binaryAddressFromAddress, binaryAddressFromPrincipal, hexAddressFromPrincipal
+} from "azle/canisters/ledger"
+
+import { hashCode } from "hashcode";
+import { v4 as uuidv4} from "uuid";
---
+++ src/types/hashcode.d.ts
@@ -0,0 +1,4 @@
+// hashcode.d.ts
+declare module 'hashcode' {
+    export function hashCode(): any; // Replace 'any' with more specific types if known
+}
