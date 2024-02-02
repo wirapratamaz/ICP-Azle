50d8b970a1e61d6b2b043d5ac2833d06de9e3f06
wirapratamaz <wirabagus185@gmail.com>
2024-02-02 21:19:11 +0800

    feat: add defining record and variant types

--- src/dfinity_js_backend/src/index.ts
+++ src/dfinity_js_backend/src/index.ts
@@ -6,3 +6,48 @@ import {
 
 import { hashCode } from "hashcode";
 import { v4 as uuidv4} from "uuid";
+
+// defining record and variant types
+// * It contains basic properties that are needed to define a product.
+const Product = Record({
+    id: text,
+    title: text,
+    description: text,
+    price: nat64,
+    location: text,
+    attachmentURL: text,
+    soldAmount: nat64,
+    seller: Principal,
+});
+
+const ProductPayload = Record({
+    title: text,
+    description: text,
+    price: nat64,
+    location: text,
+    attachmentURL: text,
+});
+
+const OrderStatus = Variant({
+    PaymentPending: text,
+    PaymentComplete: text,
+});
+
+const Order = Record({
+    id: text,
+    product: Product,
+    buyer: Principal,
+    status: OrderStatus,
+    createdAt: text,
+    updatedAt: text,
+    paid_at_block: Opt(nat64), // optional record the block when payment was made
+    memo: nat64,
+});
+
+const Message = Variant({
+    NotFound: text,
+    InvalidPayload: text,
+    PaymentFailed: text,
+    PaymentComplete: text,
+});
+
